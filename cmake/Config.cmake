3c987d3e51bba1813c133c61427fee0b5a0feea6
Mahmut ALEMDAR <mahmuttalemdar@outlook.com>
2021-01-31 19:35:42 +0300

    Updated version to v1.0.1

--- cmake/Config.cmake
+++ cmake/Config.cmake
@@ -6,8 +6,8 @@ set(BUILD_ID 001 CACHE STRING "Build ID which is comming from pipelines.")
 
 set(VER_MAJ 1)      # App major version number
 set(VER_MIN 0)      # App minor version number
-set(VER_PAT 0)      # App patch version number
-set(VER_BID 0)      # Pipeline build number
+set(VER_PAT 1)      # App patch version number
+set(VER_BID 1)      # Pipeline build number
 
 if(DEFINED ENV{BUILD_ID})
     set(VER_BID $ENV{BUILD_ID})
